589823d7cd4785cd91d6ce1dc68e7ecb43bfc5e1
pulchnymamut <pulchnymamut@gmail.com>
2022-05-19 23:29:17 +0200

    API request to get data

---
+++ main.js
@@ -0,0 +1,11 @@
+var request = new XMLHttpRequest();
+let data = {};
+
+function requestListener() {
+    data = this.responseText;
+    console.log(data);
+}
+
+request.addEventListener("load", requestListener);
+request.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json");
+request.send();
