dcd0ad685364ce5c3bed21e2b699a6737743c485
pulchnymamut <pulchnymamut@gmail.com>
2022-05-22 16:29:06 +0200

    data displays

--- main.js
+++ main.js
@@ -12,18 +12,27 @@ request.send();
 
 // D3
 
-const w = 500;
-const h = 100;
+const w = 800;
+const h = 300;
+const width = 25;
 
-
+const dataset = [10, 12, 14, 22, 8, 4];
 
 
 const visual = d3.select("#visHolder")
     .append("svg")
     .attr("width", w)
     .attr("height", h)
+    .selectAll("rect")
+    .data(dataset)
+    .enter()
     .append("rect")
-    .attr("height", 100)
-    .attr("width", 25)
-    .attr("x", 0)
+    .attr("class", "bar")
+    .attr("width", width)
+    .attr("height", (d) => d*10)
+    .attr("x", (d, i) => {
+        return i*(width + 5)
+    })
     .attr("y", 0)
+    .style("height", (d) => d)
+
