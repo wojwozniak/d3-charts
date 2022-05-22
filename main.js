cc06bdcffaac5d9bba999e4e6edf89f823f569a3
pulchnymamut <pulchnymamut@gmail.com>
2022-05-22 17:12:12 +0200

    rewrote api contact, added hover style

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-let request = new XMLHttpRequest();
-let data = {};
-
-function requestListener() {
-    data = this.responseText;
-    console.log(data);
-}
-
-request.addEventListener("load", requestListener);
-request.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json");
-request.send();
 
 // D3
-
 const w = 800;
 const h = 300;
-const width = 25;
+const width = 1.4;
+const scale = 0.01;
 
-const dataset = [10, 12, 14, 22, 8, 4];
+console.log(10);
+
+let response = d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+    .then((data) => {
+        const dataset = data.data;
+        console.log(dataset);
+        const visual = d3.select("#visHolder")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("width", width)
+            .attr("height", (d) => {
+                return d[1] * scale;
+        })
+        .attr("x", (d, i) => {
+            return i*(width + 1)
+        })
+        .attr("y", (d, i) => {
+            return h-d[1]*scale;
+        })
+        //.style("height", (d) => {
+        //    console.log(d[1])
+        //    return d[1];
+       // })  
+})
 
 
-const visual = d3.select("#visHolder")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("width", width)
-    .attr("height", (d) => d*10)
-    .attr("x", (d, i) => {
-        return i*(width + 5)
-    })
-    .attr("y", 0)
-    .style("height", (d) => d)
-
+//h-d*scale
