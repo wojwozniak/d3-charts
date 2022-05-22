8e79bfc4d8678f7c55791db7a7b5600b90586685
pulchnymamut <pulchnymamut@gmail.com>
2022-05-22 16:16:35 +0200

    d3 display working

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ request.addEventListener("load", requestListener);
 request.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json");
 request.send();
 
+// D3
+
+const w = 500;
+const h = 100;
+
+
+
+
+const visual = d3.select("#visHolder")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .append("rect")
+    .attr("height", 100)
+    .attr("width", 25)
+    .attr("x", 0)
+    .attr("y", 0)
