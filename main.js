c6f66354197e77272be4c2a6513e85fe32cdefbd
pulchnymamut <pulchnymamut@gmail.com>
2022-05-27 21:25:30 +0200

    Fixed X scale, displayed cells

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 let response = d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json")
     .then((data) => {
-        console.log(data);
+        const dataset = data.monthlyVariance;
+        console.log(dataset);
 
         // Chart constants
-        const w = 760;
-        const h = 330;
+        const w = 1340;
+        const h = 530;
 
         // X axis
         const xScale = d3.scaleTime()
-            .domain([new Date("01-01-1753"), new Date("01-01-2015")])
+            .domain([new Date("1753-01-01"), new Date("2015-01-01")])
             .range([40, w - 20]);
         
         const xAxis = d3.axisBottom()
             .scale(xScale)
             .tickFormat(d3.timeFormat("%Y"))
-            .tickSize(10);
+            .tickSize(1);
         
         
         // Displaying chart
@@ -23,16 +24,25 @@ let response = d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectRe
             .attr("width", w)
             .attr("height", h)
             .selectAll("rect")
-            .data(data)
+            .data(dataset)
             .enter()
             .append("rect")
+            .attr("class", "cell")
+            .attr("width", 5)
+            .attr("height", 33)
+            .attr("x", (d, i) => {
+                return 40 + (d.year-1752)*5;
+            })
+            .attr("y", (d) => {
+                return d.month * 33;
+        })
         
         
         // Calling axes
-        const axisB = d3.select("svg")
+         const axisB = d3.select("svg")
             .append("g")
             .call(xAxis)
-            .attr("id", "x-axis");
+            .attr("id", "x-axis"); 
         
         
     }
