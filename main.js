3bb116fcd57eb0382d34abc85c13dc8c79f72cec
pulchnymamut <pulchnymamut@gmail.com>
2022-05-27 22:21:41 +0200

    Both axes added, colour scale added

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
+// Color scale from darkest to brightest
+const colorScale = ["#313695", "#717AB9", "#ABD9E9", "#E0F3F8", "#FFFFBF", "#FEE090", "#FBAD61", "#FDAE61", "#D53028"]
+
+// API response
 let response = d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json")
     .then((data) => {
         const dataset = data.monthlyVariance;
         console.log(dataset);
 
         // Chart constants
-        const w = 1340;
+        const w = 1380;
         const h = 530;
+        const barHeight = 33;
+        const barWidth = 5;
+
+        // Calculating color to display
+        let variances = [];
+        for (let i = 0; i < dataset.length; i++) {
+            variances.push(dataset[i].variance);
+        }
+        let colors = variances.map((v) => {
+            if (v < -1.800) {
+                return colorScale[0];
+            } else if (v < -1.275) {
+                return colorScale[1];
+            } else if (v < -0.750) {
+                return colorScale[2];
+            } else if (v < -0.225) {
+                return colorScale[3];
+            } else if (v < 0.225) {
+                return colorScale[4];
+            } else if (v < 0.750) {
+                return colorScale[5];
+            } else if (v < 1.275) {
+                return colorScale[6];
+            } else if (v < 1.800) {
+                return colorScale[7];
+            } else return colorScale[8];
+        });
 
         // X axis
         const xScale = d3.scaleTime()
@@ -15,7 +46,24 @@ let response = d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectRe
         const xAxis = d3.axisBottom()
             .scale(xScale)
             .tickFormat(d3.timeFormat("%Y"))
-            .tickSize(1);
+            .tickSize(10);
+        
+        // Y Axis
+
+        const yScale = d3.scaleBand()
+            .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+            .range([barHeight, 430]);
+        
+        const yAxis = d3.axisLeft()
+            .scale(yScale)
+            .tickValues(yScale.domain())
+            .tickFormat((month) => {
+                let date = new Date(0);
+                date.setUTCMonth(month);
+                let format = d3.timeFormat('%B');
+                return format(date);
+            })
+            .tickSize(10);
         
         
         // Displaying chart
@@ -29,13 +77,23 @@ let response = d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectRe
             .append("rect")
             .attr("class", "cell")
             .attr("width", 5)
-            .attr("height", 33)
+            .attr("height", 34)
             .attr("x", (d, i) => {
-                return 40 + (d.year-1752)*5;
+                return 40 + (d.year - 1752) * barWidth;
             })
             .attr("y", (d) => {
-                return d.month * 33;
-        })
+                return d.month * barHeight;
+            })
+            .attr("data-month", (d) => {
+                return d.month - 1;
+            })
+            .attr("data-year", (d) => {
+                return d.year;
+            })
+            .attr("data-temp", (d) => {
+                return d.variance;
+            })
+            .style("fill", (d, i) => colors[i])
         
         
         // Calling axes
@@ -44,6 +102,10 @@ let response = d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectRe
             .call(xAxis)
             .attr("id", "x-axis"); 
         
+        const axisL = d3.select("svg")
+            .append("g")
+            .call(yAxis)
+            .attr("id", "y-axis");
         
     }
 );
