b2fc7d0312cc8b4d4c3868856e816c9e54e41319
pulchnymamut <pulchnymamut@gmail.com>
2022-05-29 15:40:54 +0200

    get data from api

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
+
+// Links to data
+const EDUCATION_LINK = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json';
+const COUNTY_LINK = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json';
+
 // D3
 // Contact with API
+
+Promise.all([d3.json(COUNTY_LINK), d3.json(EDUCATION_LINK)])
+    .then((data) => renderChart(data[0], data[1]))
+    .catch((err) => console.log(err));
+  
+
+function renderChart(counties, education) {
+    console.log(counties);
+    console.log(education);
+}
