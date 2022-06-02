c54396dda82dac9eff0763ad34fe779768472509
pulchnymamut <pulchnymamut@gmail.com>
2022-06-02 13:54:36 +0200

    All the charts rendering

--- main.js
+++ main.js
@@ -39,36 +39,116 @@ const renderButtons = () => {
     kickstarter_link.textContent = "Kickstarter Chart";
 }
 
+// Color scale - from original FCC project
+const fader = (color) => d3.interpolateRgb(color, '#fff')(0.2);
+const color =  d3.scaleOrdinal().range(
+    [
+      '#1f77b4',
+      '#aec7e8',
+      '#ff7f0e',
+      '#ffbb78',
+      '#2ca02c',
+      '#98df8a',
+      '#d62728',
+      '#ff9896',
+      '#9467bd',
+      '#c5b0d5',
+      '#8c564b',
+      '#c49c94',
+      '#e377c2',
+      '#f7b6d2',
+      '#7f7f7f',
+      '#c7c7c7',
+      '#bcbd22',
+      '#dbdb8d',
+      '#17becf',
+      '#9edae5'
+    ].map(fader)
+);
+
+// Chart constants
+const treemap = d3.treemap().size([960, 500]);
+const svg = d3.select("svg");
+
+// D3
+function renderChart(data) {
+    // Clear data before rendering new chart
+    svg.selectAll("*").remove();
+
+    // Root of diagram
+    const root = d3
+        .hierarchy(data)
+        .eachBefore((d) => {
+            d.data.id = (d.parent ? d.parent.data.id + '.' : '') + d.data.name;
+        })
+        .sum((d) => d.value)
+        .sort((a, b) => {
+            return b.height - a.height || b.value - a.value;
+        });
+    treemap(root);
+    
+    // Render group (which will contain rect and text)
+    const cell = svg
+        .selectAll('g')
+        .data(root.leaves())
+        .enter()
+        .append('g')
+        .attr('class', 'group')
+        .attr('transform', (d) => {
+        return 'translate(' + d.x0 + ',' + d.y0 + ')';
+        });
+    
+    // Render rect element
+    cell
+        .append('rect')
+        .attr('id', (d) => d.data.id)
+        .attr('class', 'tile')
+        .attr('width', (d) => d.x1 - d.x0)
+        .attr('height', (d) => d.y1 - d.y0)
+        .attr('data-name', (d) => d.data.name)
+        .attr('data-category', (d) => d.data.category)
+        .attr('data-value', (d) => d.data.value)
+        .attr('fill', (d) => color(d.data.category));
+    
+    // Render text
+    cell
+        .append('text')
+        .attr('class', 'label')
+        .selectAll('tspan')
+        //Regex making it work, snippet from main project
+        .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+        .enter()
+        .append('tspan')
+        .attr('x', 4)
+        .attr('y', (d, i) => 13 + i * 10)
+        .text((d) => d);
+}
+
 // Main function
 function main() {
     // Default dataset
-    dataset = videogames.children;
+    dataset = videogames;
 
     //Render buttons and default chart
     renderButtons();
     renderChart(dataset);
 }
 
-// D3
-function renderChart(dataset) {
-    console.log(dataset);
-}
-
 // Click listeners - setting data to selected by user, displaying correct title and subtitle and then calling renderChart function
 videogames_link.onclick = () => {
-    dataset = videogames.children;
+    dataset = videogames;
     title.textContent = "Video Game Sales";
     subtitle.textContent = "Top 100 Most Sold Video Games by Platform"
     renderChart(dataset);
 }
 movies_link.onclick = () => {
-    dataset = movies.children;
+    dataset = movies;
     title.textContent = "Movies Box Office";
     subtitle.textContent = "Top 100 Movies with Highest Gross Income"
     renderChart(dataset);
 }
 kickstarter_link.onclick = () => {
-    dataset = kickstarter.children;
+    dataset = kickstarter;
     title.textContent = "Kickstarter Top Funded Projects";
     subtitle.textContent = "Top 100 Most Funded Kickstarter Projects"
     renderChart(dataset);
