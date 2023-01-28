036dbd0252886a729406f1f2cad58000fac14f0f
Wojtek <pulchnymamut@gmail.com>
2023-01-28 17:50:49 +0100

    base file structure

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script lang="ts">
+
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-3xl font-bold text-green-500">
+      How to install Tailwind CSS 3 in Nuxt 3
+    </h1>
   </div>
 </template>
