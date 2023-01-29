1d92cb1246e21cbb2ac2ee11307a6d2d5072f658
Wojtek <pulchnymamut@gmail.com>
2023-01-29 14:43:49 +0100

    preparing to push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 export default function defineNuxtConfig() {
   return {
+    target: 'static',
+    router: {
+      base: '/d3-collection/'
+    },
     css: [
       '@/assets/css/main.css',
     ],
